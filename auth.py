a27066444d89082c9f2b3609f40baf03e741b1ef
Josh Gordon <github@joshgordon.net>
2020-04-09 21:34:17 -0400

    Fix issue when a DN is reused.

--- auth.py
+++ auth.py
@@ -1,6 +1,10 @@
 from beefcafe_auth.models import Certificate
 def get_user(dn):
-    user = Certificate.objects.get(dn=dn).user
+    certs = Certificate.objects.filter(dn=dn) 
+    if not certs:
+        return None
+    # todo: make sure all users are the same under the list of certs. Shouldn't ever be a problem.
+    user = certs[0].user
     user_dict = {'username': user.username, 'first_name': user.first, 'last_name': user.last}
     return user_dict
 
